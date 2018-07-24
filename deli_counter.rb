c7314d493ea0d5e954479409f90806dde8f7933d
Karen C <chenyanhuan66@163.com>
2018-07-24 01:55:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,17 @@
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
+  else
+    num = katz_deli.size
+    array = []
+    array << "The line is currently: "
+    index = 1
+    while num > 0
+      array << "#{index}. #{katz_deli[index-1]} " 
+      num -= 1
+      index += 1
+    end
+    return array.join
   end
+  
 end
